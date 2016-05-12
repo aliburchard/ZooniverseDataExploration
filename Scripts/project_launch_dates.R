c132383801e896195a0d1f8eb05149b64a99411c
aliburchard <ali@zooniverse.org>
2016-05-12 15:40:52 +0100

    testing gitignore

---
+++ Scripts/project_launch_dates.R
@@ -0,0 +1,6 @@
+## Getting Project Release Data
+
+# download updated csv at: https://docs.google.com/spreadsheets/d/1hMmQxmaw-dNljQHek5f8TjUP4RlXp2a_Pkhcfc0Nr9o/edit#gid=0
+
+# dates are MM/DD/YYYY, annoyingly.
+
